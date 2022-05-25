607eb4dbbe210a2b78791dd134606cb4f454675f
bilgi <bilgi@DESKTOP-MS151D4>
2022-05-25 15:12:08 +0300

    Database Bağlantısı gerçekleştirildi

---
+++ src/java/dao/DBConnection.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author can
+ */
+public  abstract class DBConnection {
+    private Connection connection;
+
+    public Connection getConnection() {
+        if(this.connection == null){
+            try {
+            Class.forName("org.postgresql.Driver");
+            this.connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/cmd","admin", "12345");
+            System.out.println("Bağlantı Başarılı");
+
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        }
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+}
